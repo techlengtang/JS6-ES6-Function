961f3ff4912d75795f03a3a0691c73222d0776cd
techlengtang <techlengtang01@gmail.com>
2025-02-11 14:52:16 +0700

    Add files via upload

---
+++ ex2 - Clone with spread.js
@@ -0,0 +1,31 @@
+// THE CONTEXT
+// The originalArray contains some elements.
+// The function updateArray takes an array, an index, and a new value as parameters, and  update the value at the specified index in the array.
+
+// THE PROBLEM
+// - We want the original array to remain unchanged !
+
+// YOUR JOB
+//- In the function updateArray, you need to use the spread operator (...) to clone the original array before making modifications.
+
+// TO CHECK
+// Check that your code still produce the same result
+
+let originalArray = [1, 2, 3, 4, 5];
+
+// function updateArray(array, index, newValue) {
+//   array[index] = newValue;
+//   return array;
+// }
+
+function updateArray(array, index, newValue) {
+  let newArray = [...array]; // Clone the array using the spread operator
+  newArray[index] = newValue; // Modify the cloned array
+  return newArray; // Return the modified copy
+}
+
+
+let updatedArray = updateArray(originalArray, 2, 10);
+
+console.log("Original array:", originalArray); // original array should not be modified...
+console.log("Updated array:", updatedArray);
